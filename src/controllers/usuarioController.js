a37e698707167bb5d0bf9fb56247134ce504e869
Fabrizio <fabrizionapoli50@gmail.com>
2023-04-19 21:07:27 -0300

    aviso

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,14 +1,58 @@
 import Usuarios from "../Models/Usuario.js";
 
 class UsuarioController {
-    static listarUsuarios = async (req, res) => {
-        try {
-            const response = await Usuarios.find({});
-            res.status(200).json(response);
-        } catch (err) {
-            res.status(400).json({ message: `Error - ${err}`});
-        }
+  static listarUsuarios = async (req, res) => {
+    try {
+      const response = await Usuarios.find({});
+      res.status(200).json(response);
+    } catch (err) {
+      res.status(400).json({ message: `Error - ${err}` });
     }
-};
+  };
+
+  static cadastrarUsuario = async (req, res) => {
+    try {
+      let usuario = new Usuarios(req.body);
+      await usuario.save(usuario);
+      res.status(201).json(usuario);
+    } catch (err) {
+      res.status(400).json({ message: `Error - ${err}` });
+    }
+  };
+
+  static cadastrarSenhasUsuario = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const autenticacao = req.headers.user_id;
+
+      const { nomeSenha } = req.body;
+      const { senha } = req.body;
+
+      if (id === autenticacao) {
+        await Usuarios.findByIdAndUpdate(
+          id,
+          { $set: { [`senhas.${nomeSenha}`]: senha } },
+          { new: true }
+        );
+        const user = await Usuarios.findById(id);
+        res.status(200).json(user);
+      } else {
+        res.status(400).send("Você não tem autorização para isso");
+      }
+    } catch (err) {
+      res.status(400).json({ message: `Error - ${err}` });
+    }
+  };
+
+  static deletarUsuario = async (req, res) => {
+    try {
+      const { id } = req.params;
+      await Usuarios.findByIdAndDelete(id);
+      res.status(200).send("Usuario deletado");
+    } catch (err) {
+      res.status(400).json({ message: `Error - ${err}` });
+    }
+  };
+}
 
 export default UsuarioController;
--- src/routes/routesUsuario.js
+++ src/routes/routesUsuario.js
@@ -5,5 +5,9 @@ const Router = express.Router();
 
 Router
     .get("/usuarios", UsuarioController.listarUsuarios)
+    .post("/usuarios", UsuarioController.cadastrarUsuario)
+    .put("/usuarios/:id", UsuarioController.cadastrarSenhasUsuario)
+    .delete("/usuarios/:id", UsuarioController.deletarUsuario)
+
 
 export default Router;
