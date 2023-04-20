36e7c87fd15e7aa22955322d08c92eb647fa77ed
Fabrizio <fabrizionapoli50@gmail.com>
2023-04-19 21:04:05 -0300

    esconder dados sensiveis

--- src/config/dbconnect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect("mongodb+srv://fabrizioestudarprog:napoli510@login-usuarios.5smtbd7.mongodb.net/ModeloLogin")
-
-const db = mongoose.connection;
-
-export default db;
